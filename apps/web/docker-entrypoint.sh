d0b5f6b2fec8653e2987a38933dae5173df88aa0
Shanmukha <shanmukha.17022006@gmail.com>
2026-09-05 00:40:23 +0530

    fix(web): use nginx resolver to prevent startup crash when API is deploying

--- apps/web/docker-entrypoint.sh
+++ apps/web/docker-entrypoint.sh
@@ -5,7 +5,8 @@ set -e
 export NGINX_PORT="${PORT:-80}"
 export API_HOST="${API_HOST:-api}"
 export API_PORT="${API_PORT:-3000}"
-envsubst '$NGINX_PORT $API_HOST $API_PORT' \
+export RESOLVER=$(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2}' /etc/resolv.conf)
+envsubst '$NGINX_PORT $API_HOST $API_PORT $RESOLVER' \
   < /etc/nginx/conf.d/default.conf.template \
   > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
